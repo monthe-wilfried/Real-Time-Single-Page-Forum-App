446f31d58557e7290048fb7e26dca4f59b0f5855
Monthe Wilfried <monthetcham@yahoo.fr>
2020-06-02 21:57:15 +0200

    JWT Laravel Package

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +53,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof TokenInvalidException){
+            return response()->json(['error' => 'Token is Invalid'], 400);
+        }
+        elseif  ($exception instanceof TokenExpiredException) {
+            return response()->json(['error' => 'Token is Expired'], 400);
+        }
+        elseif  ($exception instanceof JWTException ) {
+            return response()->json(['error' => 'Token is Absent'], 400);
+        }
         return parent::render($request, $exception);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-///*
-//|--------------------------------------------------------------------------
-//| API Routes
-//|--------------------------------------------------------------------------
-//|
-//| Here is where you can register API routes for your application. These
-//| routes are loaded by the RouteServiceProvider within a group which
-//| is assigned the "api" middleware group. Enjoy building your API!
-//|
-//*/
-//
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
+
 
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
 Route::post('{reply}/like', 'LikeController@like');
 Route::delete('{reply}/like', 'LikeController@unlike');
+
+
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+
+});
