e6714bc47e6d64b6a2b8c2912764df675768bf2c
Monthe Wilfried <monthetcham@yahoo.fr>
2020-06-03 16:09:20 +0200

    Login Component and login

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -2,19 +2,20 @@
     <v-card
         color="grey lighten-4"
         flat
-        height="200px"
         tile
     >
         <v-toolbar bottom>
 
             <v-toolbar-title>Forum <sup>Dev.</sup></v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-toolbar-items >
-                <v-btn flat>Forum</v-btn>
-                <v-btn flat>Ask Question</v-btn>
-                <v-btn flat>Category</v-btn>
-                <v-btn flat>Login</v-btn>
-            </v-toolbar-items>
+            <div>
+                <v-btn text>Forum</v-btn>
+                <v-btn text>Ask Question</v-btn>
+                <v-btn text>Category</v-btn>
+                <router-link to="/login">
+                    <v-btn text>Login</v-btn>
+                </router-link>
+            </div>
         </v-toolbar>
     </v-card>
 </template>
