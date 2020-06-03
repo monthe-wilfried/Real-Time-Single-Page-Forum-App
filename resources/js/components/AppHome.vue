257a5c7b7c770a356598e69b2d9541b273f7e266
Monthe Wilfried <monthetcham@yahoo.fr>
2020-06-03 14:17:32 +0200

    Vuetify, Vue Router and initial components

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Toolbar from './Toolbar'
+    import AppFooter from './AppFooter'
+
+    export default {
+        components:{Toolbar, AppFooter}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card
+        color="grey lighten-4"
+        flat
+        height="200px"
+        tile
+    >
+        <v-toolbar bottom>
+
+            <v-toolbar-title>Forum <sup>Dev.</sup></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items >
+                <v-btn flat>Forum</v-btn>
+                <v-btn flat>Ask Question</v-btn>
+                <v-btn flat>Category</v-btn>
+                <v-btn flat>Login</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
